56646cbb558013ab61d35f347641f5a76aaa4852
Patrick José de Castro Amaral <patrick@MacBook-Air-de-Patrick.local>
2019-07-09 19:08:31 -0300

    ícone

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-repositories',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoriesComponent implements OnInit {
 
-  constructor() { }
+  fiveUsers: object;
+  threeUsers: object;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.get5Users().subscribe(data => {
+      this.fiveUsers = data;
+    });
+    this.data.get3Users().subscribe(data => {
+      this.threeUsers = data;
+    });
   }
 
 }
